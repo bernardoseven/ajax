3a62a1df9f8e521502717f6f0cc3786bf8e8a9de
bernardoseven <bernardoseven@gmail.com>
2017-12-11 14:25:04 +0000

    ajax full functioning

--- app/controllers/finishes_controller.rb
+++ app/controllers/finishes_controller.rb
@@ -1,2 +1,25 @@
 class FinishesController < ApplicationController
+  before_action :setup_task, only: [:create, :destroy]
+  
+  def create
+    @task.active_task
+    respond_to do |format|
+      format.html { redirect_to tasks_path }
+      format.js
+    end
+  end
+  
+  def destroy
+    @task.inactive_task
+    respond_to do |format|
+      format.html { redirect_to tasks_path }
+      format.js
+    end
+  end
+  
+  private
+  def setup_task
+    @task = Task.find_by(params[:id])
+  end
+  
 end
